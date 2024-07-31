4a16d4d38d0fe005b61a84733b77b2a2c378e88b
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-07-31 16:37:34 +0200

    logika do pobierania przepisów po kategorii oraz paginacja

--- controllers/recipes/get.js
+++ controllers/recipes/get.js
@@ -39,4 +39,30 @@ const getCategory = async (req, res) => {
     res.status(500).json({ message: "Error fetching categories" });
   }
 };
-module.exports = { getMainrecipesByCategory, getCategory };
+
+const getRecipesByCategory = async (req, res) => {
+  const limit = 8;
+  const { page = 1 } = req.query;
+  const offset = (page - 1) * limit;
+  try {
+    const { category } = req.params;
+    const recipies = await Recipes.find({ category }).skip(offset).limit(limit);
+
+    if (!recipies.length) {
+      return res
+        .status(404)
+        .json({ message: `No recipes found for category ${category}` });
+    }
+
+    res.json(recipies);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching recipies by category" });
+  }
+};
+
+module.exports = {
+  getMainrecipesByCategory,
+  getCategory,
+  getRecipesByCategory,
+};
