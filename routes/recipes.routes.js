a11dac074a81d353fb55382e487f97621831f541
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-01 11:20:47 +0200

    nowy endpoint - recipe/:id

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -7,10 +7,12 @@ const {
   getMainrecipesByCategory,
   getCategory,
   getRecipesByCategory,
+  getRecipeById,
 } = require("../controllers/recipes/get");
 
 recipesRouter.get("/main-page", getMainrecipesByCategory);
 recipesRouter.get("/category-list", getCategory);
 recipesRouter.get("/:category", getRecipesByCategory);
+recipesRouter.get("/recipe/:id", getRecipeById);
 
 module.exports = { recipesRouter };
