17fadf2fe11fcb778d1cf8b259cde9ca9cadf77d
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-02 11:41:20 +0200

    dodany endpoint /search

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -9,11 +9,14 @@ const {
   getMainrecipesByCategory,
   getCategory,
   getRecipesByCategory,
+  searchRecipe,
 } = require("../controllers/recipes/get");
 
 recipesRouter.get("/main-page", getMainrecipesByCategory);
 recipesRouter.get("/category-list", getCategory);
+recipesRouter.get("/search", searchRecipe);
 recipesRouter.get("/:category", getRecipesByCategory);
 recipesRouter.get("/recipe/:id", getRecipeById);
+
 
 module.exports = { recipesRouter };
