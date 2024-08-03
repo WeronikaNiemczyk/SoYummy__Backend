1b6d4a67a9028219bd75c6ce0f04ac6ef134cd9d
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-03 13:50:15 +0200

    update routes

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -11,12 +11,12 @@ recipesRouter.get("/search", recipesController.searchRecipe);
 recipesRouter.get("/:category", recipesController.getRecipesByCategory);
 recipesRouter.get("/recipe/:id", recipesController.getRecipeById);
 
-recipesRouter.post("/ownRecipes", recipesController.addOwnRecipe);
+recipesRouter.get("/ownRecipes/user", auth, recipesController.getOwnRecipes);
+recipesRouter.post("/ownRecipes", auth, recipesController.addOwnRecipe);
 recipesRouter.delete(
   "/ownRecipes/:id",
   auth,
   recipesController.deleteOwnRecipe
 );
-recipesRouter.get("/ownRecipes", auth, recipesController.getOwnRecipes);
 
 module.exports = { recipesRouter };
