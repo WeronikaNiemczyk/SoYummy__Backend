1b97abf90192ba42a6c22e1a32c156a342dad3cb
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2024-08-03 11:51:16 +0200

    utworzone dodawanie nowego przepisu

---
+++ controllers/recipes/addOwnRecipe.js
@@ -0,0 +1,15 @@
+// controllers/recipes/addOwnRecipe.js
+
+const Recipes = require("../../models/recipe.model");
+
+const addOwnRecipe = async (req, res) => {
+  try {
+    const newRecipe = new Recipes({ ...req.body, owner: req.user_id });
+    const savedRecipe = await newRecipe.save();
+    res.status(201).json(savedRecipe);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding recipe" });
+  }
+};
+
+module.exports = addOwnRecipe;
